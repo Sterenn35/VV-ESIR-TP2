47d552024ea6aeb93499f686691351048f3eca2a
Sterenn35 <slh35@hotmail.fr>
2023-12-08 11:54:17 +0100

    Update javaparser

--- code/javaparser-starter/src/main/java/fr/istic/vv/PublicElementsPrinter.java
+++ code/javaparser-starter/src/main/java/fr/istic/vv/PublicElementsPrinter.java
@@ -4,11 +4,21 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.*;
 import com.github.javaparser.ast.visitor.VoidVisitorWithDefaults;
 
+import java.util.HashSet;
+import java.util.Set;
+
 
 // This class visits a compilation unit and
 // prints all public enum, classes or interfaces along with their public methods
 public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
 
+    private Set<String> getters;
+    private Set<String> attributes;
+
+    public PublicElementsPrinter() {
+        getters = new HashSet<>();
+        attributes = new HashSet<>();
+    }
     @Override
     public void visit(CompilationUnit unit, Void arg) {
         for(TypeDeclaration<?> type : unit.getTypes()) {
@@ -18,14 +28,20 @@ public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
 
     public void visitTypeDeclaration(TypeDeclaration<?> declaration, Void arg) {
         if(!declaration.isPublic()) return;
-        System.out.println(declaration.getFullyQualifiedName().orElse("[Anonymous]"));
         for(MethodDeclaration method : declaration.getMethods()) {
             method.accept(this, arg);
         }
         // Printing nested types in the top level
         for(BodyDeclaration<?> member : declaration.getMembers()) {
-            if (member instanceof TypeDeclaration)
+            if (member instanceof FieldDeclaration) // FieldDeclaration comprend la déclaration des champs de la classe (attributs)
                 member.accept(this, arg);
+        }
+        // On va retourner uniquement les attributs privés qui n'ont pas de getters
+        for(String attribute : attributes) {
+            String method = "get"+attribute+"()";
+            if (!getters.contains(method)) {
+                System.out.println("Voici les attributs privés qui n'ont pas de getter :" +attribute);
+            }
         }
     }
 
@@ -42,7 +58,16 @@ public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
     @Override
     public void visit(MethodDeclaration declaration, Void arg) {
         if(!declaration.isPublic()) return;
-        System.out.println("  " + declaration.getDeclarationAsString(true, true));
+        String[] mots = declaration.getDeclarationAsString(true, true).split(" ");
+        getters.add(mots[2].toLowerCase());
+    }
+
+    public void visit(FieldDeclaration declaration, Void arg) {
+        String[] mots = declaration.toString().split(" ");
+        if(mots[0].equals("private")) {
+            mots[2] = mots[2].substring(0, mots[2].length() - 1);
+            attributes.add(mots[2]);
+        }
     }
 
 }
