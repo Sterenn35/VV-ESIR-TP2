99498939e33d1c4ba0ef1bff71490faa20d8b170
adenlexar <96285929+adenlexar@users.noreply.github.com>
2023-12-17 17:26:42 +0100

    Add comments

--- code/javaparser-starter/src/main/java/fr/istic/vv/NoGetterVisitor.java
+++ code/javaparser-starter/src/main/java/fr/istic/vv/NoGetterVisitor.java
@@ -14,7 +14,7 @@ import java.util.Set;
 public class NoGetterVisitor extends VoidVisitorWithDefaults<Void> {
     public static String resultatTotal = ""; //la string du rapport final
     public String resultat; //la string du rapport local à une classe
-    private Set<String> getters; //stocke les noms des méthodes getters
+    private Set<String> getters; //stocke les noms les méthodes publiques
     private Set<String> attributes; //stocke les noms des attributs privés
     private String className; //stocke le nom du package et de la classe étudiée
 
@@ -60,7 +60,7 @@ public class NoGetterVisitor extends VoidVisitorWithDefaults<Void> {
         this.writeResult(); //on écrit notre résultat de la classe dans le doc global
 
         for (BodyDeclaration<?> classes: declaration.getMembers()) {
-            if (classes instanceof ClassOrInterfaceDeclaration) { //Si on rentre dans une classe on recrée un Visiteur et on refait le même traitement sur la classe fille
+            if (classes instanceof ClassOrInterfaceDeclaration) { //Si on rentre dans une classe on refait le même traitement sur la classe fille
                 getters = new HashSet<>();
                 attributes = new HashSet<>();
                 className = "";
@@ -75,14 +75,7 @@ public class NoGetterVisitor extends VoidVisitorWithDefaults<Void> {
         visitTypeDeclaration(declaration, arg);
     }
 
-    @Override
-    public void visit(MethodDeclaration declaration, Void arg) {
-        if(!declaration.isPublic()) return;
-        String[] mots = declaration.getDeclarationAsString(true, true).split(" ");
-        getters.add(mots[2].toLowerCase());
-    }
-
-    public void visit(FieldDeclaration declaration, Void arg) {
+    public void visit(FieldDeclaration declaration, Void arg) { //récupère le nom de l'attribut privé
         String[] mots = declaration.toString().split(" ");
         if(mots[0].equals("private")) {
             mots[2] = mots[2].substring(0, mots[2].length() - 1);
@@ -90,8 +83,15 @@ public class NoGetterVisitor extends VoidVisitorWithDefaults<Void> {
         }
     }
 
+    @Override
+    public void visit(MethodDeclaration declaration, Void arg) { //récupère les méthodes publiques
+        if(!declaration.isPublic()) return; //si la méthode est privée on ne la retient pas
+        String[] mots = declaration.getDeclarationAsString(true, true).split(" ");
+        getters.add(mots[2].toLowerCase());
+    }
+
     /**
-     * Récupère les résultats du parser (les attributs) et
+     * Concatène le résultat local à une classe au résultat global
      */
     public void writeResult() {
         System.out.println("Writing...");
